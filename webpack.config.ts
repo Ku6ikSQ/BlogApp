d6a625ebd0555e0cb6d073989edc2671745dd18d
Timofey <tikhonow66@gmail.com>
2023-08-10 17:36:26 +0500

    fix: fixed invalid imports

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,7 +3,7 @@ import {
     type IDevServer,
     type IPaths,
     buildConfig,
-} from "./configs/wwebpack";
+} from "./configs/webpack";
 import { type Configuration } from "webpack";
 import path from "path";
 
